06d752d1268b94a59fd7ebc3b427ac6166f78dc8
Damon Davison <damon@curatur.com>
2016-07-23 15:10:24 -0400

    Add HomeController with report button

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,64 @@
+var token = "AIzaSyAm7jyIIqtaJRJoEOBPLjKF_Bq8iV2KCpM"
+
+$(function(){
+  function initGeolocation() {
+    if (navigator && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    } else {
+      console.log('Geolocation is not supported');
+    }
+  }
+
+  function errorCallback() {
+    console.log("Browser geolocation API failure");
+    console.dir(arguments);
+  }
+
+  function successCallback(position) {
+    var location = position.coords.latitude + ',' + position.coords.longitude;
+
+    var geoUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + location + "&key=" + token;
+
+    $.get(geoUrl, function(response) {
+      var address = response["results"][0]["formatted_address"];
+      var place_id = response["results"][0]["place_id"];
+
+      var payload = {
+        "location" : {
+          "latitude": position.coords.latitude,
+          "longitude": position.coords.longitude,
+          "address": address,
+          "google_place_id": place_id
+        }
+      };
+
+      $('#report').click(function(event) {
+        event.preventDefault();
+        $.post("/locations", payload, function(){
+          console.dir(arguments);
+        });
+      });
+
+      var mapUrl = "http://maps.google.com/maps/api/staticmap?";
+      mapUrl = mapUrl + 'center=' + location;
+      mapUrl = mapUrl + '&zoom=15&size=512x512';
+      mapUrl = mapUrl + '&maptype=roadmap&sensor=false';
+      mapUrl = mapUrl + '&markers=color:red|label:Y|' + location;
+      mapUrl = mapUrl + "&key=" + token;
+
+      // console.log(mapUrl);
+
+      var addressElement = document.getElementById("address");
+      var imgElement = document.getElementById("static-map");
+
+      addressElement.innerHTML = address;
+      imgElement.src = mapUrl;
+    }).fail(function(){
+      console.log("Failure");
+      console.dir(arguments);
+    });
+  }
+
+  initGeolocation();
+});
+
