7bbed8dbc140f5cf6057d9ec49bb2feb7c706a2b
Damon Davison <damon@curatur.com>
2016-07-25 23:16:16 -0400

    Remove locations index route for security

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
-  resources :locations
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :locations, only: [:create, :show]
 end
